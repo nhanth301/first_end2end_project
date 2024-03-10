c3a88e9c56a8158619c21521a685d793c3476d96
nhanth301 <nhanthien.tnn@gmail.com>
2024-03-10 23:04:26 +0700

    first_commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from src.logger.logging import logging
+
+logging.info("this is my test")
+
+logging.info("this is second testing")
+logging.info("this is second testing")
+logging.info("this is second testing")
+logging.info("this is second testing")

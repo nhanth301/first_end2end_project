133bd02c02c7737fcbeaa527d2b9cce51e78c5a4
nhanth301 <nhanthien.tnn@gmail.com>
2024-03-11 11:46:31 +0700

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nhanthien3011-test",
+    version="0.0.1",
+    author="Nguyen Thien Nhan",
+    author_email="nhanthien.tnn@gmail.com",
+    url="https://github.com/nhanth301",
+    description="An application that informs you of the time in different locations and timezones",
+    packages=find_packages()
+)
